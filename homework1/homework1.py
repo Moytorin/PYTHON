5080f719c4de1ef97fde206e78d2d8fc2aeb94bc
Mikhail <moytorin@bk.ru>
2022-12-15 16:56:15 +0300

    homework1

---
+++ homework1/homework1.py
@@ -0,0 +1,47 @@
+
+# # Task 2
+# num = int(input("Enter a number: ")) # 238
+# a = num // 100 # 2
+# b = num // 10  # 23
+# c = b % 10     # 3
+# d = num % 10   # 8
+# sum = a + c + d
+# print("sum=", (sum))
+
+# # Task 4
+# num = int(input('Enter a number: '))  
+# d = num//3  
+# k = d*2
+# p = d//2
+# s = d//2
+# print(p, k, s)
+
+# # Task 6
+# num = int(input("Enter a number of ticket: ")) #123456
+# num1 = num % 1000 # 456
+# a = num // 1000   # 123
+# b = a // 100        # 1
+# c = a % 10        # 3
+# d = a % 100       # 23
+# f = d // 10       # 2  
+# b1 = num1 // 100     
+# c1 = num1 % 10        
+# d1 = num1 % 100       
+# f1 = d1 // 10
+# if (b + c + f) == (b1 + c1 + f1):
+#     print('You winner!', b,c,f, b1, c1, f1)
+# else:
+#     print('Will u win next time', b,c,f, b1, c1, f1)
+# print(num1)
+# print(a)
+
+# # Task 8 
+# m = int(input('m = '))
+# n = int(input('n = '))
+# ras = m * n
+# print('Rasmer= ',ras)
+# k = int(input('k = '))
+# if ras % k == 0:
+#     print('chokolate is good')
+# else:
+#     print('poprobyu ewe raz')
