7b4f21afa66a71a019181b94d6341e7f3e25d5a1
Mikhail <moytorin@bk.ru>
2023-01-09 22:24:28 +0300

    2 zad

--- homework4/homework4.py
+++ homework4/homework4.py
@@ -14,30 +14,87 @@
 # Output: 11 6
 # 6 12
 
-n = int(input('Введите кол-во элементов первого набора: '))
-m = int(input('Введите кол-во элементов второго набора: '))
+# n = int(input('Введите кол-во элементов первого набора: '))
+# m = int(input('Введите кол-во элементов второго набора: '))
+# import random
+# n_array = []
+# m_array = []
+# for i in range(n):
+#     n_array.append(random.randint(0, n))
+# print(n_array)
+# for j in range(m):
+#     m_array.append(random.randint(0, m))
+# print(m_array)
+# c = []
+# for i in n_array:
+#     for j in m_array:
+#         if i == j:
+#             c.append(i)
+#             break
+# c.sort()    
+# print(c)
+# list2 = []
+# for item in c:
+#     if item not in list2:
+#         list2.append(item)
+# print(list2)
+
+
+
+
+
+
+# Задача 24
+# В фермерском хозяйстве в Карелии выращивают чернику. Она растет на круглой грядке, причем кусты высажены 
+# только по окружности. Таким образом, у каждого куста есть ровно два соседних. Всего на грядке растет N кустов. 
+# Эти кусты обладают разной урожайностью, поэтому ко времени сбора на них выросло различное число ягод – 
+# на i-ом кусте выросло ai ягод.
+# В этом фермерском хозяйстве внедрена система автоматического сбора черники. Эта система состоит из управляющего 
+# модуля и нескольких собирающих модулей. Собирающий модуль за один заход, находясь непосредственно перед некоторым 
+# кустом, собирает ягоды с этого куста и с двух соседних с ним.
+
+# Напишите программу для нахождения максимального числа ягод, которое может собрать за один заход собирающий 
+# модуль, находясь перед некоторым кустом заданной во входном файле грядки.
+
+# Input: 4
+# (значения сгенерированы случайным образом
+# 4 2 3 1 )
+
+# Output: 9
+
+m = int(input('Введите кол-во кустов: '))
 import random
-n_array = []
 m_array = []
-for i in range(n):
-    n_array.append(random.randint(0, n))
-print(n_array)
-for j in range(m):
-    m_array.append(random.randint(0, m))
+for item in range(m):
+    m_array.append(random.randint(1, 10))
 print(m_array)
+
+max_ = m_array[0]
+for item in m_array:
+    if item > max_:
+         max_ = item
 c = []
-for i in n_array:
-    for j in m_array:
-        if i == j:
-            c.append(i)
-            break
-c.sort()    
+c.append(max_)
+m_array.remove(max_)
+
+max_ = m_array[0]
+for item in m_array:
+    if item > max_:
+         max_ = item
+c.append(max_)
+m_array.remove(max_)
+
+max_ = m_array[0]
+for item in m_array:
+    if item > max_:
+         max_ = item
+c.append(max_)
+m_array.remove(max_)
+
 print(c)
-list2 = []
-for item in c:
-    if item not in list2:
-        list2.append(item)
-print(list2)
+
+result = c[0] + c[1] + c[2]
+print(result)
 
 
 
